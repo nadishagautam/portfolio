224738e523344849e96d0f2831e7408a51ac74c0
nadishagautam <nadishagautam@gmail.com>
2024-08-18 20:41:22 -0400

    ui fixes

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./landing.css";
 import nadishaGif from "../../assets/images/Nadisha.GIF";
 
 const Landing = () => {
+  useEffect(() => {
+    document.body.classList.add("landing-page");
+
+    return () => {
+      document.body.classList.remove("landing-page");
+    };
+  }, []);
+
   return (
     <div className="container">
       <img src={nadishaGif} className="nadisha-gif" />
